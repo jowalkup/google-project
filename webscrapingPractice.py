f3332cb70dc0df1975e88c3d3a00552b71836992
jowalkup <jwalkup40@stuy.edu>
2023-03-14 19:32:48 -0400

    got list of voting sites from gov website

--- webscrapingPractice.py
+++ webscrapingPractice.py
@@ -7,4 +7,9 @@ req = requests.get(URL)
 soup = BeautifulSoup(req.text, "html.parser")
 #for link in soup.find_all('a'):
     #print(link.get('href')) # gets the links
-print(soup.get_text()) # gets the text
+#print(soup.get_text()) # gets the text
+
+URL2 = "https://www.nyc.gov/site/civicengagement/voting/poll-site-service-list.page"
+req2 = requests.get(URL2)
+soup2 = BeautifulSoup(req2.text, "html.parser")
+print(soup2.get_text())
