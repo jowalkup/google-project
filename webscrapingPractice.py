18c883ce2755f83cde017b988d59b11159f0f6f2
jowalkup <jwalkup40@stuy.edu>
2023-03-14 10:48:25 -0400

    extract all urls from a page

--- webscrapingPractice.py
+++ webscrapingPractice.py
@@ -1,5 +1,9 @@
 #install beautiful soup, requests, and html5lib first! any url should work
+from bs4 import BeautifulSoup
 import requests
 URL = "https://konstantinnovation.github.io/apcsSpring.html"
-r = requests.get(URL)
-print(r.content)
+req = requests.get(URL)
+print(req.content)
+soup = BeautifulSoup(req.text, "html.parser")
+for link in soup.find_all('a'):
+    print(link.get('href'))
