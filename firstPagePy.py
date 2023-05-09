f875fa21fa90fc965aba947105e6bd82879d4931
jowalkup <jwalkup40@stuy.edu>
2023-05-09 14:47:00 -0400

    made python file that will make html file

---
+++ firstPagePy.py
@@ -0,0 +1,43 @@
+func = open("firstPage.html", 'w')
+
+
+name = []
+address = []
+zip_code = []
+language = []
+
+with open('soup1.txt') as f:
+    counter = 0;
+    for line in f:
+        if(line.strip() != ""):
+            if(counter%4 == 0):
+                name.append(line.strip())
+            elif(counter%4 == 1):
+                address.append(line.strip())
+            elif(counter%4 == 2):
+                zip_code.append(line.strip())
+            else:
+                language.append(line.strip())
+            counter += 1
+
+f.close()
+
+def wrap(arr):
+    final = ""
+    start = "<br><tr>"
+    end = "</tr>"
+    for x in arr:
+        final += start + x + end
+    return final
+
+names = wrap(name)
+
+func.write('''<html>
+    <head>
+    <title>Names</title>
+    <h1>Names</h1>
+    </head>
+    <table> <tr>''' + names + '''</tr></table>
+    </html>''')
+
+func.close()
