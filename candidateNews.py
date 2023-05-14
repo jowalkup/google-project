a698e8f0867b78a5cb75e54dd9393e6d3112d21d
mlee40 <mlee40@stuy.edu>
2023-05-14 01:40:36 -0400

    candidate news

--- candidateNews.py
+++ candidateNews.py
@@ -1,5 +1,23 @@
 from pygooglenews import GoogleNews
 
-gn = GoogleNews()
-search = gn.search('lockdown')
-print(search)
+def get_news(search):
+    gn = GoogleNews(country = 'US')
+    search = gn.search(search)
+    newsitem = search['entries']
+
+    title = []
+    link = []
+
+    for item in newsitem:
+        title.append(item.title)
+        link.append(item.link)
+
+    news = list(zip(title, link))
+
+    for item in news:
+        print(item)
+        print()
+    print(len(newsitem))
+    return
+
+get_news('Kathy C. Hochul')
